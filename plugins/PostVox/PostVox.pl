d672d7fe3e982ccb72c2524e35547741f14fa0e2
bchoate <bchoate@3dc1ab89-750f-0410-b951-f7fd8a356519>
2006-11-13 04:29:32 +0000

    Updates to store user+blog level settings for authentication. Changed method of storing apilink for posted entries. Convert formatting of entry to HTML prior to submitting. Post extended entry text if available.
    git-svn-id: http://code.sixapart.com/svn/mtplugins/trunk/PostVox@110 3dc1ab89-750f-0410-b951-f7fd8a356519

--- plugins/PostVox/PostVox.pl
+++ plugins/PostVox/PostVox.pl
@@ -2,15 +2,17 @@ package MT::Plugin::PostVox;
 
 use strict;
 use warnings;
+
 use MT 3.3;
 
 use constant NS_DC => 'http://purl.org/dc/elements/1.1/';
+
 use base 'MT::Plugin';
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 my $plugin = MT::Plugin::PostVox->new({
-    name            => 'Post to the Vox',
-    description     => 'Cross post to the Vox',
+    name            => 'Post to Vox',
+    description     => 'Automatic cross-posting to Vox.',
     author_name     => 'Six Apart, Ltd.',
     author_link     => 'http://www.sixapart.com/',
     version         => $VERSION,
@@ -18,9 +20,9 @@ my $plugin = MT::Plugin::PostVox->new({
         ['vox_username'],
         ['vox_password'],
         ['vox_url'],
-        ['always_post']
+        ['always_post'],
     ]),
-    config_template => 'config.tmpl',
+    blog_config_template => 'config.tmpl',
     callbacks       => {
         'CMSPostSave.entry' => {
             priority => 9,
@@ -39,10 +41,30 @@ sub add_input_field {
     my ($eh, $app, $tmpl) = @_;
     my $q = $app->param;
     my $blog_id = $q->param('blog_id');
-    my $config = $plugin->get_config_hash('blog:'.$blog_id);
-    my $checked = "checked" if $config->{always_post};
+    my $config = $plugin->get_config_hash('blog:'.$blog_id.':user:'.$app->user->id);
+    return unless ( $config->{vox_username} || $config->{vox_password} );
 
-    $$tmpl =~ s!(<div class="button-bar">)!<p><input type="checkbox" name="post_to_vox" $checked/> <MT_TRANS phrase="Cross post to the Vox"></p>\n$1!is;
+    my $entry_id = $app->param('id');
+    my $entry;
+    if ($entry_id) {
+        $entry = MT::Entry->load($entry_id, { cached_ok => 1 });
+    }
+    my $already_posted = $entry && (($entry->tangent_cache || '') =~ m!http://www.vox.com!);
+
+    my $checked = "checked=\"checked\""
+        if $config->{always_post} || $already_posted;
+    my $label;
+    if ($already_posted) {
+        if ($entry->tangent_cache =~ m!(http://www.vox.com\S+)!i) {
+            my $apilink = $1;
+            if ($apilink =~ m!/asset_id=([a-e0-9]+)!) {
+                my $asset_id = $1;
+                $label = $plugin->translate('Update <a href="[_1]" target="_blank">Vox post</a>', "http://www.vox.com/compose/#id:$asset_id");
+            }
+        }
+    }
+    $label ||= '<MT_TRANS phrase="Cross-post to Vox">';
+    $$tmpl =~ s!(<div class="button-bar">)!<p><input type="checkbox" id="post_to_vox" name="post_to_vox" $checked /> <label for="post_to_vox">$label</label></p>\n$1!is;
     1; 
 }
 
@@ -50,22 +72,24 @@ sub hdlr_post_save {
     my ($cb, $app, $obj, $orig) = @_;
     my $q = $app->param;
     my $blog_id = $q->param('blog_id');
-    my $config = $plugin->get_config_hash('blog:'.$blog_id);
+    my $config = $plugin->get_config_hash('blog:'.$blog_id.':user:'.$app->user->id);
 
-    return $obj unless ( $config->{vox_username} || $config->{vox_password} || $config->{vox_url} );
+    return $obj unless ( $config->{vox_username} && $config->{vox_password} && $config->{vox_url} );
+
+    require MT::Entry;
     return $obj if $obj->status != MT::Entry::RELEASE();
     return $obj unless $q->param('post_to_vox');
 
     # APILINK
-    my $apilink;
+    my $apilink = $config->{vox_apilink};
     my $new_post = 1;
-    if ( $obj->keywords && $obj->keywords =~ m!http://www.vox.com!i) {
-        $apilink = $obj->keywords;
+    if ( $obj->tangent_cache && $obj->tangent_cache =~ m!(http://www.vox.com\S+)!i) {
+        $apilink = $1;
         $new_post = 0;
-    } else {
+    } elsif (!$apilink) {
         my $url = $config->{vox_url};
-        if ( $url !~ m!rsd\.xml$!i ) {
-            if ( $url =~ m!/$!i ) {
+        if ( $url !~ m!rsd\.xml$! ) {
+            if ( $url =~ m!/$! ) {
                 $url .= 'rsd.xml';
             } else {
                 $url .= '/rsd.xml';
@@ -75,15 +99,31 @@ sub hdlr_post_save {
             $url = 'http://'.$url;
         }
         $apilink = $plugin->_find_apilink_rsd( $url );
+        if ($apilink) {
+            $config->{vox_apilink} = $apilink;
+            $plugin->set_config_value('vox_apilink', $apilink, 'blog:'.$blog_id.':user:'.$app->user->id);
+        }
         return $obj unless $apilink;
     }
 
     # ENTRY
-    use XML::Atom::Entry;
+    require XML::Atom::Entry;
     my $enc = MT->instance->config('PublishCharset') || undef;
     my $entry = XML::Atom::Entry->new;
     $entry->title( MT::I18N::encode_text( $obj->title , 'utf-8', $enc ) );
-    $entry->content( MT::I18N::encode_text( $obj->text, 'utf-8', $enc ) );
+
+    # Apply filters to entry text and extended entry if available
+    my $text = $obj->text;
+    my $text_more = $obj->text_more;
+    $text = '' unless defined $text;
+    $text_more = '' unless defined $text_more;
+    my $filters = $obj->text_filters;
+    push @$filters, '__default__' unless @$filters;
+    $text = MT->apply_text_filters($text, $filters);
+    $text_more = MT->apply_text_filters($text_more, $filters) if $text_more ne '';
+    $text .= "\n\n" . $text_more if $text_more ne '';
+
+    $entry->content( MT::I18N::encode_text( $text, 'utf-8', $enc ) );
 
     my @tags = $plugin->_get_entry_tags( $obj );
     my $dc = XML::Atom::Namespace->new( dc => NS_DC );
@@ -92,7 +132,7 @@ sub hdlr_post_save {
     }
 
     # CLIENT
-    use XML::Atom::Client;
+    require XML::Atom::Client;
     my $api = XML::Atom::Client->new;
     $api->username( $config->{vox_username} );
     $api->password( $config->{vox_password} );
@@ -100,17 +140,18 @@ sub hdlr_post_save {
     # SEND
     if ( $new_post ) {
         my $edit_uri = $api->createEntry( $apilink, $entry )
-            or return $plugin->log({
+            or return MT->log({
                 message => $api->errstr,
             });
-
         # Save EditURI
-        $obj->keywords( $edit_uri );
+        my @t = split /\s+/, ($obj->tangent_cache || '');
+        push @t, $edit_uri;
+        $obj->tangent_cache( join ' ', @t );
         $obj->save;
     } else {
         #FIXME:Edit not support
         my $ret = $api->updateEntry( $apilink, $entry )
-            or return $plugin->log({
+            or return MT->log({
                 message => $api->errstr,
             });
     }
@@ -129,7 +170,7 @@ sub _find_apilink_rsd {
     my $xml     = XML::Simple::XMLin( $xml_data );
     my $apilink = $xml->{service}->{apis}->{api}->{Atom}->{apiLink};
 
-    return 0, $plugin->log({
+    return 0, MT->log({
         message => "Couldn't retrieve 'apiLink' from $xml",
     }) unless $apilink;
 
@@ -140,7 +181,7 @@ sub _get_entry_tags {
     my $self = shift;
     my($entry) = @_;
 
-    return '' unless $entry;
+    return () unless $entry;
 
     require MT::ObjectTag;
     require MT::Entry;
@@ -156,5 +197,37 @@ sub _get_entry_tags {
     return @tags;
 }
 
-1;
+# Since these settings are blog *and* user specific, automatically assign
+# attach the user id to the scope element of all load/save/reset operations
+# that operate on the blog scope.
 
+sub load_config {
+    my $plugin = shift;
+    my $app = MT->instance;
+    return unless $app->can('user');
+
+    my ($param, $scope) = @_;
+    $scope .= ':user:' . $app->user->id if $scope =~ m/^blog:/;
+    $plugin->SUPER::load_config($param, $scope);
+}
+
+sub save_config {
+    my $plugin = shift;
+    my $app = MT->instance;
+    return unless $app->can('user');
+
+    my ($param, $scope) = @_;
+    $scope .= ':user:' . $app->user->id if $scope =~ m/^blog:/;
+    $plugin->SUPER::save_config($param, $scope);
+}
+
+sub reset_config {
+    my $plugin = shift;
+    my $app = MT->instance;
+    return unless $app->can('user');
+    my ($scope) = @_;
+    $scope .= ':user:' . $app->user->id if $scope =~ m/^blog:/;
+    $plugin->SUPER::reset_config($scope);
+}
+
+1;
